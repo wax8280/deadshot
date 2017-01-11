baca3858fdb1fdae0e919bd4b9f9f80a8c646234
wax8280 <wax8280@163.com>
2017-01-11 14:59:25 +0800

    rebuild

---
+++ lib/deadshot_log.py
@@ -0,0 +1,66 @@
+# !/usr/bin/env python
+# coding: utf-8
+
+import logging
+import os
+import sys
+from logging.handlers import WatchedFileHandler
+from setting import DEADSHOT_LOG_PATH
+from functools import partial
+
+LOG_PATH = DEADSHOT_LOG_PATH
+
+
+class VLogging(object):
+    logger_dict = {}
+
+    @staticmethod
+    def log(name, message):
+        VLogging.get_logger(name).info(message)
+
+    @staticmethod
+    def get_logger(name):
+
+        failed_path = os.path.join(LOG_PATH, name)
+
+        if not os.path.exists(failed_path):
+            os.makedirs(failed_path)
+
+        if name not in VLogging.logger_dict:
+            logger = logging.getLogger(name)
+
+            formatter = logging.Formatter(
+                '[%(asctime)s][' + name + '] %(message)s')
+            file_handler = WatchedFileHandler(
+                os.path.join(failed_path, name + '.log'))
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+            logger.setLevel(logging.INFO)
+
+            VLogging.logger_dict[name] = logger
+        return VLogging.logger_dict[name]
+
+
+class UsualLogging(VLogging):
+    info_mes = '[INFO] {message}'
+    error_mes = '[ERROR] {message}'
+    waring_mes = '[WARING] {message}'
+
+    def __init__(self, name):
+        self._name = name
+        self._logger = partial(VLogging.log, name=self._name)
+
+    def info(self, message):
+        self._logger(
+            message=self.info_mes.format(message)
+        )
+
+    def error(self, message):
+        self._logger(
+            message=self.error_mes.format(message)
+        )
+
+    def waring(self, message):
+        self._logger(
+            message=self.waring_mes.format(message)
+        )
