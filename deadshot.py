04b8926106317546859b6e0c0fbc7b7eca55b089
wax8280 <wax8280@163.com>
2017-03-21 11:22:46 +0800

    reload yaml

--- deadshot.py
+++ deadshot.py
@@ -5,7 +5,7 @@ from deadshot.lib.deadshot_log import UsualLogging
 from deadshot.lib.send_email import make_report, send_mail
 import traceback
 import requests
-from deadshot.config import config
+import deadshot.config
 from deadshot.shoters import RetryShot, SupervisorShot, UnknowShot
 from deadshot.callbacks import *
 from flask import Flask
@@ -51,12 +51,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def slave():
+    reload(deadshot.config)
     p = DeadShot(
         supervisor_shot_ctx=(
-            {'log_path': config['RETRY_LOG_PATH'],
-             'filter_dirname_list': config['RETRY_LOG_FILTER_DIR'],
-             'filter_filename_list': config['RETRY_LOG_FILTER_FILE']}),
-        unknowshot_shot_ctx=({'log_path': config['UNKNOWN_LOG_PATH']}),
+            {'log_path': deadshot.config.config['RETRY_LOG_PATH'],
+             'filter_dirname_list': deadshot.config.config['RETRY_LOG_FILTER_DIR'],
+             'filter_filename_list': deadshot.config.config['RETRY_LOG_FILTER_FILE']}),
+        unknowshot_shot_ctx=({'log_path': deadshot.config.config['UNKNOWN_LOG_PATH']}),
     )
     result = p.run()
     return json.dumps(result)
@@ -68,22 +69,22 @@ def master():
 
     p = DeadShot(
         supervisor_shot_ctx=(
-            {'log_path': config['RETRY_LOG_PATH'],
-             'filter_dirname_list': config['RETRY_LOG_FILTER_DIR'],
-             'filter_filename_list': config['RETRY_LOG_FILTER_FILE']}),
-        unknowshot_shot_ctx=({'log_path': config['UNKNOWN_LOG_PATH']}),
+            {'log_path': deadshot.config.config['RETRY_LOG_PATH'],
+             'filter_dirname_list': deadshot.config.config['RETRY_LOG_FILTER_DIR'],
+             'filter_filename_list': deadshot.config.config['RETRY_LOG_FILTER_FILE']}),
+        unknowshot_shot_ctx=({'log_path': deadshot.config.config['UNKNOWN_LOG_PATH']}),
     )
     ctxs.append(p.run())
 
-    for server_name, server_ip in config['SLAVE_IP'].items():
+    for server_name, server_ip in deadshot.config.config['SLAVE_IP'].items():
         try_time = 0
-        while try_time < config['MASTER_REQUSET_RETRY_TIME']:
+        while try_time < deadshot.config.config['MASTER_REQUSET_RETRY_TIME']:
             try:
-                res = requests.get('http://' + server_ip + ':' + str(config['SLAVE_PORT']))
+                res = requests.get('http://' + server_ip + ':' + str(deadshot.config.config['SLAVE_PORT']))
             except Exception as e:
                 traceback.print_exc()
                 try_time += 1
-                if try_time == config['MASTER_REQUSET_RETRY_TIME']:
+                if try_time == deadshot.config.config['MASTER_REQUSET_RETRY_TIME']:
                     error_message += "Reach the maximum retry.Server name:{}.\nErrInfo:{}\n".format(server_name, e)
                 continue
 
@@ -97,7 +98,7 @@ def master():
 
 
 if __name__ == '__main__':
-    if config['TYPE'] == 'slave':
-        app.run(host='0.0.0.0', port=config['SLAVE_PORT'])
-    elif config['TYPE'] == 'master':
+    if deadshot.config.config['TYPE'] == 'slave':
+        app.run(host='0.0.0.0', port=deadshot.config.config['SLAVE_PORT'])
+    elif deadshot.config.config['TYPE'] == 'master':
         master()
